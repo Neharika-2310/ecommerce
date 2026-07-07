aa8c2d14bcae925df629e6fbae489cd669cd647d
Neharika <23neharikasingh@gmail.com>
2026-07-07 11:32:29 +0530

    expiry coupon inngest bug fiexd

--- gocart/app/api/store/product/route.js
+++ gocart/app/api/store/product/route.js
@@ -1,5 +1,6 @@
 import imagekit from "@/configs/imageKit"
-import prisma from "@/configs/prisma"
+
+import { prisma } from "@/lib/prisma";
 import authSeller from "@/middlewares/authseller"
 import { getAuth } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
--- gocart/middlewares/authseller.js
+++ gocart/middlewares/authseller.js
@@ -1,5 +1,5 @@
-import { prisma } from "@/configs/prisma";
 
+import { prisma } from "@/lib/prisma";
 const authSeller = async (userId) => {
     try {
         const user = await prisma.user.findUnique({
